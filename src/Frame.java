6478080043513bcfdbcfc1912c6cf6c8b26ce8df
sarah du <sdu17@deerfield.edu>
2016-02-09 15:03:38 -0500

    CSV reader, can read one column of the csv file and print, java frame, driver classes

---
+++ src/Frame.java
@@ -0,0 +1,27 @@
+import java.awt.*;
+import javax.swing.*;
+	
+public class Frame extends JFrame
+{
+	  private Container c;
+
+	  public Frame()
+	  {
+		c = this.getContentPane();
+	  }
+
+	  public void addPanel(JPanel p){
+	    p.setPreferredSize(new Dimension(700,700));
+	    //color background
+	    p.setBackground(new Color(248,248,255));
+	    //add panel to frame
+	    c.add(p);
+	  }
+
+	  public void showFrame(){
+	    this.pack();
+	    this.setVisible(true);
+	    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	  }
+
+}
